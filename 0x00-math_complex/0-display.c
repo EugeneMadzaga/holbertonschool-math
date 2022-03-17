1bd15e6fada79c7a3ac321b6ed54ce886f751a17
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-03-17 08:39:52 +0100

    fix 0 display

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,40 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * display_complex_number - print number complex
- * @c: structura
- */
+* display_complex_number - Display complex number
+* @c: Complex number
+*/
+
 void display_complex_number(complex c)
 {
-	char s = (c.im < 0) ? '-' : '+';
-	double s_im = (c.im < 0) ? -(c.im) : c.im;
-
-	if (c.im == 0)
+	if (c.re == 0)
 	{
-		printf("%.9g\n", c.re);
-	}
-	else if (c.re == 0)
-	{
-		if (c.im > 0)
-		{
-			if (c.im == 1)
-				printf("i\n");
-			else
-				printf("%.9gi\n", s_im);
-		}
+		if (c.im < 0)
+			printf("- %.9gi\n", c.im * (-1));
+		else if (c.im == 0)
+			printf("0\n");
 		else
-		{
-			if (c.im == -1)
-				printf("-i\n");
-			else
-				printf("%c %.9gi\n", s, s_im);
-		}
+			printf("%.9gi\n", c.im);
 	}
-	else if (c.re != 0 && c.im != 0)
+	else
 	{
-		if (s_im == 1)
-			printf("%.9g %c i\n", c.re, s);
+		if (c.im < -1)
+			printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+		else if (c.im == -1)
+			printf("%.9g - i\n", c.re);
+		else if (c.im == 0)
+			printf("%.9g\n", c.re);
+		else if (c.im == 1)
+			printf("%.9g + i\n", c.re);
 		else
-			printf("%.9g %c %.9gi\n", c.re, s, s_im);
+			printf("%.9g + %.9gi\n", c.re, c.im);
 	}
 }
