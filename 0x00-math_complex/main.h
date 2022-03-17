e829bfef87f1eb478a6cd9ca6f2bb4579b64595a
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-03-17 08:37:16 +0100

    fix main file

--- 0x00-math_complex/main.h
+++ 0x00-math_complex/main.h
@@ -1,24 +1,18 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <math.h>
+#include <stdarg.h>
 
 /**
- * struct complex_number - Struct with the elements
- *
- * @re: real member
- * @im: imaginary member
- *
- * Description: structure with the imaginary number
- * members
- */
-struct complex_number
+* struct complex - Structure
+*/
+
+typedef struct complex
 {
 	double re;
 	double im;
-};
-typedef struct complex_number complex;
+} complex;
 
 void display_complex_number(complex c);
 complex conjugate(complex c);
@@ -28,6 +22,6 @@ void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
-void complex_from_mod_arg (double m, double arg, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
 
 #endif
