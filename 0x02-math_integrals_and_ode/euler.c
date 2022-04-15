88a6ef9fad2cb3ec359f3524566bece895276a5a
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-04-16 00:02:19 +0200

    fix euler file

--- 0x02-math_integrals_and_ode/euler.c
+++ 0x02-math_integrals_and_ode/euler.c
@@ -9,19 +9,19 @@
  *@x0: double
  *@y0: double
  *@T: double
- *@n: int 
+ *@n: int
  */
 void Euler(FILE *euler, char *name, double x0, double y0, double T, int n)
 {
-	euler = fopen("C://program.txt","name");
-	x0 = 2 * (T/n)
+	euler = fopen("C://program.txt", "name");
+	x0 = 2 * (T / n)
 	if (euler == NULL)
 	{
 		printf("Error!");
 		exit(1);
 	}
 
-	fprintf(euler, "%d",x0);
+	fprintf(euler, "%d", x0);
 	fclose(euler);
 }
 
